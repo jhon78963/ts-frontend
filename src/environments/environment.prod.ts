7175fcf2db97d3efa7be4342327b298485b0f47d
Jhon Livias <jhonlivias3@gmail.com>
2025-06-23 15:01:41 -0500

    fix autocomplete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://api-novedadesmaritex.zerogroups.uk/api',
+  BASE_URL: 'https://api-comercial-anderson.zerogroups.uk/api',
   BASE_FILE_URL: 'https://api-upload.zerogroups.uk/api',
   BASE_S3_URL: 'https://novedadesmaritex.s3.sa-east-1.amazonaws.com/',
   ACCESS_FILE_TOKEN: '1|sQB2jyklgp021YFX5dQBkRJCPOPWaSj3yuqObMUK94706163',
